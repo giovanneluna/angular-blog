bf1fa5b85ea9b0706c1eee71511a2910fd7e96b4
Giovanne de Luna <gikalunalticg@gmail.com>
2024-01-22 18:09:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Pontos de Exclamação Atinge 70 milhoes',
+    description:
+      'Remix do Vintage Culture, atinge a marga de mais de 70 milhoes de visitas spotify',
+    photoCover: 'https://i.ytimg.com/vi/Dn1frQB-isk/maxresdefault.jpg',
+  },
+  {
+    id: '2',
+    title: 'Liquid conhece primeiros adversários da BLAST Spring Groups',
+    description:
+      'A Liquid está no Grupo junto de FaZe, GamerLegion e Team Spirit, contra a qual estreará às 10h30 de 24 de janeiro. O campeonato em si começa no dia 22, com a participação de 16 equipes, premiação de US$ 190 mil (R$ 931,2 mil) e sendo disputado na Dinamarca. O formato da BLAST Premier Spring Groups segue o do ano passado. As equipes iniciam a LAN divididas em quatro grupos e se enfrentando em dupla eliminação com os duelos em md3. O campeão de cada chave se classificar para o Spring Final, enquanto os 2º colocados vão para o play-in, que dá mais duas vagas para o Spring Final.',
+    photoCover:
+      'https://img-cdn.hltv.org/gallerypicture/9670GnOVgJ349p0dzYfL67.jpg?ixlib=java-2.1.0&w=1200&s=903d5010ffaa5090f3bc563f83aba702',
+  },
+  {
+    id: '3',
+    title: 'VALORANT: Campeão do Champions 2023 é o novo jogador da Leviatán',
+    description:
+      'C0M é o novo jogador da Leviatán! A organização argentina anunciou a contratação do jogador nesta terça-feira (9). O campeão do Champions 2023 estará ao lado da estrela brasileira, aspas, no VCT Americas 2024.',
+    photoCover:
+      'https://noticias.maisesports.com.br/wp-content/uploads/2024/01/c0m-leviatan-valorant.jpg',
+  },
+  {
+    id: '4',
+    title: 'CBLOL 2024: LOUD oficializa a chegada de Redbert e Stardust',
+    description:
+      'A LOUD confirmou a contratação de Ygor "RedBert" para ser o novo suporte do time de League of Legends da organização na temporada 2024 do Campeonato Brasileiro (CBLOL). Ele chega para substituir Denilson "Ceos", contratado pela KaBuM, na única mudança no elenco da atual tricampeã brasileira para o ano que vem. Outra novidade é a entrada do treinador sul-coreano Seok-hee "Stardust", que comandou a LOS em 2023.',
+    photoCover:
+      'https://noticias.maisesports.com.br/wp-content/uploads/2023/12/redbert-loud-cblol-2024.jpg',
+  },
+];
